f18aa34834649762ad1443b349653d8458cf8c7c
Razu-Biswas <sreekantaraju3421@gmail.com>
2021-06-22 17:23:47 +0600

    first commit

---
+++ src/Components/Home/Home.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './Home.css'
+
+
+const Home = () => {
+    return (
+
+        <div className="bgk-img text-center">
+            <h3 className="title"> Search your jobs,</h3>
+            <h4 className="title2">or</h4>
+            <h3 className="title2">Query your team member! </h3>
+            <button type="button" class="btn btn-outline-danger">Query Your Options!</button>
+        </div>
+      
+    );
+};
+
+export default Home;
---
+++ src/Components/Navbar/Navbar.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import { Link } from "react-router-dom";
+
+const Navbar = () => {
+    return (
+        <div>
+            <nav class="navbar navbar-expand-lg navbar-light bg-danger sticky-top">
+                <div class="container-fluid">
+                    <Link class="navbar-brand" to="/">Easy Jobs</Link>
+                    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                        <span class="navbar-toggler-icon"></span>
+                    </button>
+                    <div class="collapse navbar-collapse" id="navbarNav">
+                        <ul class="navbar-nav ms-auto">
+                            <li class="nav-item">
+                                <Link class="nav-link active" aria-current="page" to="/home">Home</Link>
+                            </li>
+                            <li class="nav-item">
+                                <Link class="nav-link active" to="/login">Login</Link>
+                            </li>
+                            <li class="nav-item active">
+                                <Link class="nav-link"to="/jobs">Jobs</Link>
+                            </li>
+                            <li class="nav-item">
+                                <Link class="nav-link active" to="/Register">REgister</Link>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </nav>
+        </div>
+    );
+};
+
+export default Navbar;
